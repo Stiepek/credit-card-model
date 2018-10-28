a755314ccbeb78efbc07f85622b9f80412f8b8af
Piotr Szczepanski <StiepanR923@gmail.com>
2018-10-28 13:02:10 +0000

    block card

--- credit-card/src/test/java/pl/pszcz/creditcard/CreditCardTest.java
+++ credit-card/src/test/java/pl/pszcz/creditcard/CreditCardTest.java
@@ -14,4 +14,13 @@ public class CreditCardTest {
         //Asser / Then
         Assert.assertTrue(card.getBalance() == 2000);
     }
+    
+    @Test
+    public void allowBlockCard () {
+        CreditCard card = new CreditCard();
+        card.block();
+        
+        Assert.assertTrue(card.isBlocked());
+        
+    }
 }
